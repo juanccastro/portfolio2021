c9c18e1e3fbfe9d1c85977b905e17e9e3f78a277
Juan Camilo <jcczurg@gmail.com>
2021-09-05 03:48:24 -0400

    assasdasdasad

---
+++ build/precache-manifest.af254fc5e1a36eabe06b13ad26dbbbac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69eb9f5f8a072cf873cf24a2e1d3033e",
+    "url": "/portfolio2021/index.html"
+  },
+  {
+    "revision": "cdd874bfe33ecfdd4ae2",
+    "url": "/portfolio2021/static/css/main.0cf642cf.chunk.css"
+  },
+  {
+    "revision": "b714b98055f243ade47b",
+    "url": "/portfolio2021/static/js/2.934ec176.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio2021/static/js/2.934ec176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd874bfe33ecfdd4ae2",
+    "url": "/portfolio2021/static/js/main.84b4ef80.chunk.js"
+  },
+  {
+    "revision": "93c209e161cf1cfef73a",
+    "url": "/portfolio2021/static/js/runtime-main.ada5cba8.js"
+  },
+  {
+    "revision": "e90e662f94e435f772cf71ce5d8ffc18",
+    "url": "/portfolio2021/static/media/adidas.e90e662f.png"
+  },
+  {
+    "revision": "8d7718ac8dd3a7fd54022ef5a51e1f87",
+    "url": "/portfolio2021/static/media/complex-2.8d7718ac.png"
+  },
+  {
+    "revision": "dde7b1b5def117bcfa146fb326d991de",
+    "url": "/portfolio2021/static/media/complex.dde7b1b5.png"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/portfolio2021/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/portfolio2021/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "860f82400d4c2b7dfc5ef1cdde3b8ea9",
+    "url": "/portfolio2021/static/media/nola-2.860f8240.png"
+  },
+  {
+    "revision": "04386d2b31988a4eada69f698e9b7e1d",
+    "url": "/portfolio2021/static/media/nola.04386d2b.png"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/portfolio2021/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
